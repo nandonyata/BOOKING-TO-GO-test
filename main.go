0f29a59cc9aad5a9be052d111b2d4294bc37251e
nandonyata <nandonp02@gmail.com>
2024-04-23 14:24:02 +0700

    chore: adjust

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/nandonyata/BOOKING-TO-GO-test/config"
 )
 
+var PORT = ":3002"
+
 func main() {
 	config.ConnectDB()
 
 	r := mux.NewRouter()
-	// r.HandleFunc("/", HomeHandler)
-	// r.HandleFunc("/products", ProductsHandler)
-	// r.HandleFunc("/articles", ArticlesHandler)
-	http.Handle("/", r)
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+	})
+
+	log.Printf("Listening on port %v\n", PORT)
+	err := http.ListenAndServe(PORT, r)
+	if err != nil {
+		log.Fatalf("Error listenging on port %v, err: %v \n", PORT, err)
+	}
 
 }
